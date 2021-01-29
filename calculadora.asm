b6e7b24a739c34094317eeb9ecb9b19b7cf3d311
ricardozv <ricardosilvanet03@gmail.com>
2021-01-29 20:39:30 -0300

    code calculadora

--- calculadora.asm
+++ calculadora.asm
@@ -6,4 +6,28 @@
     r:      .asciiz "Resultado: "
     pula:   .asciiz "\n\n"
 
-.macro pula_linha 
+.macro pula_linha   # Macro pula linha 
+    li $v0, 4       # Imprimir uma string 
+    la $a0, pula    # Carrega uma string e pula em $a0
+    syscall         # Executa 
+    .end_macro      # Fim Macro 
+
+.macro menu 
+    li $v0, 4           # Imprimir uma string 
+    la $a0, menu        # Imprimi menu na tela 
+    syscall
+    li $v0, 5           # Prepara para ler um número inteiro 
+    syscall
+    move $t0, $v0       # Salva o numero digitado em $t0
+    .end_macro 
+
+.text
+.global principal 
+
+    principal:  # Funão principal 
+    menu        # Chama macro menu 
+
+# Condições 
+    beq $t0, 1, somar 
+    beq $t0, 2, subtrair 
+    beq $t0, 3, mulplicar 
